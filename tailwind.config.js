b889db6482fa658a78e522c288d2227b01d328ea
RuochenRockyWang <111890923+RuochenRockyWang@users.noreply.github.com>
2023-06-28 15:56:56 -0500

    About page (#4)
    * added a header to layout
    
    * Import 'Nunito' font and update global font-family
    
    * change href routes to lower case
    
    * add route: about
    
    * add route: about
    
    * add route: auth/login
    
    * add route: auth/register
    
    * add route: gallery
    
    * add route: purpose
    
    * add route: team
    
    * add reference for font weights from figma
    
    * initialize tailwind
    
    * add custom color to config
    
    * add theme colors to config
    
    * add tailwind
    
    * create footer component
    
    * add photos for purpose page
    
    * add photos for social page
    
    * add tailwind to globals
    
    * add footer component to layout
    
    * header + new buttons component
    
    * fixed logo resolution
    
    * feat:about page created without the subscription section
    
    * fix: replace css file with tailwind, add breakpoints for mobile view
    
    * fix:small font change (small to semibold)
    
    ---------
    
    Co-authored-by: Isaac <IsaacBlank3@gmail.com>
    Co-authored-by: pgf96 <pgfxx1@gmail.com>
    Co-authored-by: RockyWangRC <Rocky-Wang@outlook.com>

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'secondary-blue': '#98CEFF',
+        'main-blue': '#3256D5',
+        'neutral-black': '#1F1F23',
+        'neutral-white': '#F9FAF6',
+      },
+      fontSize: {
+        'heading-1': ['2.125rem',{
+          fontWeight: '700',
+          lineHeight: ''
+        }],
+      }, 
+    },
+  },
+  plugins: [],
+}
