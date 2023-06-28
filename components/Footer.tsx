11432c242ae95d83035a99448b3dd94916f864bb
Patrick Feliciano <96318160+pgf96@users.noreply.github.com>
2023-06-28 17:26:56 -0400

    Footer 1 (#5)
    * added a header to layout
    
    * Import 'Nunito' font and update global font-family
    
    * change href routes to lower case
    
    * add route: about
    
    * add route: about
    
    * add route: auth/login
    
    * add route: auth/register
    
    * add route: gallery
    
    * add route: purpose
    
    * add route: team
    
    * add reference for font weights from figma
    
    * initialize tailwind
    
    * add custom color to config
    
    * add theme colors to config
    
    * add tailwind
    
    * create footer component
    
    * add photos for purpose page
    
    * add photos for social page
    
    * add tailwind to globals
    
    * add footer component to layout
    
    * header + new buttons component
    
    * fixed logo resolution
    
    * refactor: adjust logo and text sizing for mobile. Change HTML for icons to component.
    
    * refactor: Change all icons from png to svg
    
    ---------
    
    Co-authored-by: Isaac <IsaacBlank3@gmail.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,16 @@
+import Image from "next/image"
+import Icons from "./Icons"
+import ICAF_logo from '../public/ICAF_logo_transparent.svg'
+
 const Footer = () => {
-    const socialMedia = [
-        { name: 'instagram', link: 'https://www.instagram.com' },
-        { name: 'facebook', link: 'https://www.facebook.com' },
-        { name: 'twitter', link: 'https://www.twitter.com' },
-        { name: 'youtube', link: 'https://www.youtube.com' },
-        { name: 'tiktok', link: 'https://www.tiktok.com' },
-        { name: 'linkedin', link: 'https://www.linkedin.com' },
-        { name: 'pinterest', link: 'https://www.pinterest.com' },
-    ]
     return (
         <div className='grid py-20 grid-col-1 lg:grid-cols-2  justify-center px-2 bg-neutral-100'>
-            {/* logo and address */}
             <div className='flex justify-center items-center  '>
-                <div className="flex-none w-fit pr-2 lg:pr-12">
-                    <img src='/ICAF_logo_transparent.png'></img>
+                <div className="flex-initial w-fit pr-2 lg:pr-12">
+                    <Image src={ICAF_logo} alt='ICAF logo'></Image>
                 </div>
                 <div className="w-fit">
-                    <ul className='font-normal text-md md:font-medium lg:text-lg'>
+                    <ul className='font-normal text-sm md:font-medium lg:text-lg'>
                         <li className=''>International Child Art Foundation</li>
                         <li>2549 Virginia Avenue NW,
                             <span className="block lg:inline">Washington, DC 20038</span>
@@ -24,16 +18,10 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-            {/* Social media */}
-            <div className="flex justify-center items-center mt-3 lg:mt-0">
-                {socialMedia.map((icon, idx) => (
-                    <a className='' key={icon.name} href={icon.link} target="_blank" rel="noopener noreferrer">
-                        <img className={idx === socialMedia.length - 1 ? 'inline-block px-3' : 'inline-block px-3'} src={`social/${icon.name}.png`} alt={`${icon.name}`} />
-                    </a>
-                ))}
+            <div className='flex justify-center items-center mt-9 lg:mt-0'>
+                <Icons />
             </div>
         </div>
-
     )
 }
 export default Footer
