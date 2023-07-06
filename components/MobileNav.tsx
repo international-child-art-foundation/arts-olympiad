01d686282e429afa366d8d36aaf81ae7366ffc4f
Isaac <IsaacBlank3@gmail.com>
2023-07-06 12:49:37 -0700

    refactor: MobileNav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,76 +1,4 @@
 'use client'
-
-// saving for now in case we don't like the react-burger-menu and want a custom implementation
-
-// import React, { useState } from 'react';
-// import Link from 'next/link';
-
-
-
-// const MobileNav = () => {
-//     const [isOpen, setIsOpen] = useState(false);
-
-//     const links = [
-//         { name: 'Home', url: '/' },
-//         { name: 'Gallery', url: '/gallery' },
-//         { name: 'Purpose', url: '/purpose' },
-//         { name: 'About', url: '/about' },
-//         { name: 'Team', url: '/team' }
-//     ]
-
-//     const handleClick = (event: React.MouseEvent) => {
-//         event.stopPropagation();
-//         setIsOpen(!isOpen);
-//     };
-    
-//     const closeMenu = () => {
-//         setIsOpen(false);
-//     };
-
-//     return(
-//         <div onClick={closeMenu} className="flex flex-col justify-center items-center">
-//             <button onClick={handleClick} 
-//             className="flex flex-col justify-center items-center">
-//             <span className={`bg-black block transition-all duration-300 ease-out 
-//                             h-0.5 w-6 rounded-sm ${isOpen ? 
-//                             'rotate-45 translate-y-1' : '-translate-y-0.5'
-//                             }`} >
-//             </span>
-//             <span className={`bg-black block transition-all duration-300 ease-out 
-//                             h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 
-//                             'opacity-0' : 'opacity-100'
-//                             }`} >
-//             </span>
-//             <span className={`bg-black block transition-all duration-300 ease-out 
-//                             h-0.5 w-6 rounded-sm ${isOpen ? 
-//                             '-rotate-45 -translate-y-1' : 'translate-y-0.5'
-//                             }`} >
-//             </span>    
-//             </button>
-
-//             {isOpen && 
-//                 <div className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center">
-//                     <div className="bg-black p-4 w-full h-1/2">
-//                         <button onClick={closeMenu} className="top-0 right-0 m-2">
-//                             <img src="close.svg" alt="Close" className="h-6 w-6" />
-//                         </button>
-//                         {links.map(link => (
-//                             <Link key={link.name} href={link.url}
-//                                 className="block py-1 text-white no-underline">{link.name}
-//                             </Link>
-//                         ))}
-//                     </div>
-//                 </div>
-//             }
-
-//         </div>
-
-//     );
-// };
-
-// export default MobileNav;
-
-
 import React, {useState, useEffect} from 'react';
 import { slide as Menu } from 'react-burger-menu'
 import Link from 'next/link';
@@ -93,26 +21,6 @@ const MobileNav = () => {
     const handleStateChange = (state : {isOpen: boolean }) => {
         setIsOpen(state.isOpen);
     };
-
-    // due to conflicting state implementing the feature where clicking outside the menu closes the menu it interferes with the built in state
-    // for closing using the cross icon TBD which is important or if there is an easy fix  
-
-    // useEffect(() => {
-    //     const closeMenuIfClickedOutside = (event: MouseEvent) => {
-    //         const target = event.target as HTMLElement;
-    //         if (
-    //             isOpen && 
-    //             !target.closest('.bm-menu') && 
-    //             !target.closest('.bm-burger-button')
-    //         ) {
-    //             closeMenu();
-    //         }
-    //     };
-    //     document.addEventListener('click', closeMenuIfClickedOutside);
-        
-    //     return () => document.removeEventListener('click', closeMenuIfClickedOutside);
-    // }, [isOpen]); // Re-run the effect when isOpen changes
-
 
     return (
         <div className='md:hidden'>
