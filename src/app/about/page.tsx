98302bada41db8c1e5070ebbe4b74959a9cd5b14
RockyWangRC <Rocky-Wang@outlook.com>
2023-07-07 01:51:24 -0500

    fix:adjust layout for about page and import imgs

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,12 @@
 import ContentContainer from "../../../components/ui/ContentContainer";
 import Image from "next/image";
+import groupImage from "../../../public/about/about.png";
+import icon1 from "../../../public/about/icon1.png";
+import icon2 from "../../../public/about/icon2.png";
+import icon3 from "../../../public/about/icon3.png";
+import icon4 from "../../../public/about/icon4.png";
+import linkIcon from "../../../public/about/link.png";
+import { link } from "fs";
 
 export default function aboutPage() {
   return (
@@ -10,49 +17,53 @@ export default function aboutPage() {
       </div>
 
       <Image 
-        src="/about/about.png" 
+        src={groupImage}
         className="w-full h-auto" 
         alt="Group of children holding up art pieces in the national mall." 
       />
       {/* 4 content containers */}
       <div className="flex flex-col items-center bg-secondary-blue">
         <ContentContainer>
-          <Image 
-            src="/about/icon1.png" 
-            className="w-auto h-auto max-w-[100px] max-h-[100px] mb-6 md:mb-0 md:mr-6" 
-            alt="Artistic depection of a person with black hair and a multi-colored shirt."
-          />
-          <div>
+          <div className="w-1/5 mb-6 ml-2 mt-4">
+            <Image 
+              src={icon1}
+              alt="icon1.png"
+            />
+          </div>
+          <div className="w-5/6">
             <p className="text-base lg:text-lg">Founded in 1997, the International Child Art Foundation (ICAF) serves American children as their national arts organization and the world's children as their global arts organization.</p>
           </div>
         </ContentContainer>
         <ContentContainer>
-          <Image 
-            src="/about/icon2.png" 
-            className="w-auto h-auto max-w-[100px] max-h-[100px] mb-6 md:mb-0 md:mr-6" 
-            alt="Artistic depection of a robot holding a wrench."
-          />
-          <div>
+          <div className="w-1/5 mb-6 ml-2 mt-4">
+            <Image 
+              src={icon2}
+              alt="icon2.png"
+            />
+          </div>
+          <div className="w-5/6">
             <p className="text-base lg:text-lg">ICAF believes that children's art is arguably the most honest and pure form of human creative expression. Children worldwide produce original works of art for ICAF, and their masterpieces ICAF exhibits at prominent venues, including at The National Mall across the U.S. Capitol at the World Children's Festival, produced quadrennially by ICAF as the "Olympics" of children's imagination.</p>
           </div>
         </ContentContainer>
         <ContentContainer>
-          <Image 
-            src="/about/icon3.png" 
-            className="w-auto h-auto max-w-[100px] max-h-[100px] mb-6 md:mb-0 md:mr-6" 
-            alt="Artistic depection of a person with yellow hair and a brown dress."
-          />
-          <div>
+          <div className="w-1/5 mb-6 ml-2 mt-4">
+            <Image 
+              src={icon3}
+              alt="icon3.png"
+            />
+          </div>
+          <div className="w-5/6">
             <p className="text-base lg:text-lg">ICAF is grateful to the United States Olympic Committee for granting it an exclusive license to use the "Arts Olympiad" mark.</p>
           </div>
         </ContentContainer>
         <ContentContainer>
-          <Image 
-            src="/about/icon4.png" 
-            className="w-auto h-auto max-w-[100px] max-h-[100px] mb-6 md:mb-0 md:mr-6" 
-            alt="Artistic depection of a person with an arm sling."  
-          />
-          <div>
+        <div className="w-1/5 mb-6 ml-2 mt-4">
+            <Image 
+              src={icon4}
+              alt="icon4.png"
+            />
+          </div>
+          <div className="w-5/6">
             <p className="text-base lg:text-lg">To integrate Art and Sport with STEM disciplines, ICAF has pioneered STEAMS education for children's holistic development.</p>
           </div>
         </ContentContainer>
@@ -62,7 +73,7 @@ export default function aboutPage() {
             <a href="https://icaf.org/" className="text-base inline-flex ml-2">
               <u>International Child Art Foundation</u>
               <Image 
-                src="/about/link.png" 
+                src={linkIcon}
                 className="ml-2"
                 alt="Icon denoting a hyperlink" 
               />
@@ -70,7 +81,7 @@ export default function aboutPage() {
             <a href="https://worldchildrensfestival.org/" className="text-base inline-flex ml-2">
               <u>The World Children's Festival</u>
               <Image 
-                src="/about/link.png" 
+                src={linkIcon}
                 className="ml-2" 
                 alt="Icon denoting a hyperlink" 
               />
