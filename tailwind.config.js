833a16f0852bd29d3d965607ec196c8e65228b31
pgf96 <pgfxx1@gmail.com>
2023-07-07 18:53:40 -0400

    feat: add custom main-yellow color

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,10 @@ module.exports = {
       colors:{
         'secondary-blue': '#98CEFF',
         'main-blue': '#3256D5',
+        'main-yellow': '#FCE983',
         'neutral-black': '#1F1F23',
         'neutral-white': '#F9FAF6',
+        
       },
       fontSize: {
         'heading-1': ['2.125rem',{
