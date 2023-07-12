79ce43b0e6d539f5fe1eb268433a4cc54766df1a
pgf96 <pgfxx1@gmail.com>
2023-07-12 00:18:10 -0400

    refactor: change route to /artwork when clicking a card

--- components/ArtworkCard.tsx
+++ components/ArtworkCard.tsx
@@ -8,9 +8,7 @@ type ArtworkCardProps = {
 const ArtworkCard = ({ id, name, votes }: ArtworkCardProps) => {
   return (
     <div id={id}>
-      {/* picture placeholder */}
-      {/* /artist/{artistId}/artwork/{artworkId} */}
-      <Link href={`/gallery/${id}`}>
+      <Link href={`/artwork/${id}`}>
         <img className="w-full rounded-lg" src="https://picsum.photos/seed/picsum/300/300" alt="" />
       </Link>
       <p className="font-medium md:font-semi-bold text-lg">id: {id} </p>
