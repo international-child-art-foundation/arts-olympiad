e0f02d213e59bf6784142761a6b82375763c018c
pgf96 <pgfxx1@gmail.com>
2023-07-22 15:58:53 -0400

    feat: create temporary route and vote page

---
+++ src/app/vote/page.tsx
@@ -0,0 +1,10 @@
+
+export default function TempVotePage() {
+  return (
+    <div className="px-6 md:px-20 pt-10">
+      <div className="flex justify-center min-h-screen">
+        <h1 className="text-5xl font-semibold">Coming soon!</h1>
+      </div>
+    </div>
+  );
+}
