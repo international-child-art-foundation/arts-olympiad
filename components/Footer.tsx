a14e65b9a8fb78e0ad1b8ebdd4b6b9fb49862386
Patrick Feliciano <96318160+pgf96@users.noreply.github.com>
2023-07-28 15:19:50 -0400

    Add hover effects for navbar and social icons (#20)
    * feat: render SVGs as React Component instead of next/Image
    
    * feat: add hover effect to social media icons
    
    * feat: add hover effect to nav links and change text size/weight
    
    * feat: add hover effect to all buttons
    
    -add hover effect -change button color for non-hover and hover effects
    
    * refactor: rename 'svgIcons' to 'icons' for clarity
    
    * refactor: separate social icons to individual files for improved usability
    
    * feat: add alt text to svg components for accessability
    
    * chore: remove unused files
    
    * chore: remove unused import
    
    * feat: add hover effect to all mobile navbar items
    
    * feat: create IconLink component to handle link-related fields
    
    This change removes the hardcoded link and accessibility details from the {social}Icon.tsx component. The new IconLink component now handles link, alt, target, and rel.
    
    * refactor: delegate link and accessability feats. to IconLink component
    
    * refactor: change svg component wrapper to Link tag from IconLink component
    
    * refactor: move aria-hidden label to svg component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,30 @@
 import Image from "next/image";
-import Icons from "./Icons";
 import Link from "next/link";
 import ICAF_logo from "../public/icaf-logo-transparent.svg";
+import { InstagramIcon } from "./svgs/InstagramIcon";
+import { FacebookIcon } from "./svgs/FacebookIcon";
+import { TwitterIcon } from "./svgs/TwitterIcon";
+import { YoutubeIcon } from "./svgs/YoutubeIcon";
+import { TiktokIcon } from "./svgs/TiktokIcon";
+import { LinkedinIcon } from "./svgs/LinkedinIcon";
+import { PinterestIcon } from "./svgs/PinterestIcon";
 
+const icons = [
+  { SVGcomponent: InstagramIcon, altText:"Visit the International Child Art Foundation's Instagram page", href:"https://www.instagram.com/intlchildartfoundation/"},
+  { SVGcomponent: FacebookIcon, altText:"Visit the International Child Art Foundation's Facebook page", href:"https://www.facebook.com/ICAF.org",},
+  { SVGcomponent: TwitterIcon, altText:"Visit the International Child Art Foundation's Twitter page", href:"https://www.twitter.com/ICAF_org"},
+  { SVGcomponent: YoutubeIcon, altText:"Visit the International Child Art Foundation's Youtube page", href:"https://www.youtube.com/channel/UCvvipwdFEaNnTSv0EIhznaQ"},
+  { SVGcomponent: TiktokIcon, altText:"Visit the International Child Art Foundation's Tiktok page", href:"https://www.tiktok.com/@internationalchildartfdn"},
+  { SVGcomponent: LinkedinIcon, altText:"Visit the International Child Art Foundation's Linkedin page", href:"https://www.linkedin.com/company/international-child-art-foundation"},
+  { SVGcomponent: PinterestIcon, altText:"Visit the International Child Art Foundation's Pinterest page", href:"https://www.pinterest.com/icaf/"},
+
+
+];
 // Custom box shadows added to apply the effects of "shadow-md" with a negative offset-y
 const Footer = () => {
   return (
     <footer className="relative z-10 flex justify-center w-full shadow-[0_-4px_6px_-2px_rgba(0,0,0,0.1)] shadow-[0_-2px_4px_-2px_rgba(0,0,0,0.1)]">
-      <div className="grid py-20 grid-col-1 lg:grid-cols-2 justify-center w-full max-w-7xl">
+      <div className="grid py-20 grid-col-1 lg:grid-cols-2 justify-center px-2 w-full max-w-7xl">
         <div className="flex justify-center items-center">
           <div className="flex-initial w-fit pr-2 lg:pr-12">
             <Image src={ICAF_logo} alt="ICAF logo"></Image>
@@ -20,8 +37,24 @@ const Footer = () => {
             </address>
           </div>
         </div>
-        <div className="flex justify-center items-center mt-9 lg:mt-0">
-          <Icons />
+        <div >
+          <ol className="flex justify-center items-center mt-9 lg:mt-0">
+            {icons.map((icon,i) => (
+              <li key={i} className="inline-block px-3">
+                <Link
+                  aria-label={icon.altText}
+                  href={icon.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <icon.SVGcomponent 
+                    className="fill-neutral-black hover:fill-main-blue"
+                    aria-hidden="true"
+                  />
+                </Link>
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     </footer>
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
 
 const Button: React.FC<ButtonProps> = ({ children, mainButton,nonNav, link }) => {
   const buttonStyle = mainButton 
-    ? nonNav? "bg-blue-500 text-white px-4 py-2 rounded-full block": "bg-blue-500 text-white px-4 py-2 rounded-full hidden md:block" 
+    ? nonNav? "bg-main-blue hover:bg-secondary-blue text-white px-4 py-2 rounded-full block": "bg-main-blue hover:bg-secondary-blue text-white px-4 py-2 rounded-full hidden md:block" 
     : "bg-white text-blue-500 px-4 py-2 rounded-full hidden md:block";
 
   return (
