b854bd00556b0904c94181296d710c070ee09d42
Andrew Dame <andrewdame@google.com>
2023-07-31 23:07:10 -0400

    fix: apply mobile nav updates

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,7 +4,7 @@ import { slide as Menu } from "react-burger-menu";
 import Link from "next/link";
 
 const MobileNav = () => {
-
+  const [isOpen, setIsOpen] = useState(false);
   const links = [
     { name: "Home", url: "/" },
     { name: "Gallery", url: "/gallery" },
@@ -12,8 +12,7 @@ const MobileNav = () => {
     { name: "About ICAF", url: "/about" },
     { name: "Team", url: "/team" }
   ];
-  const [isOpen, setIsOpen] = useState(false);
-
+  
   const closeMenu = () => {
     setIsOpen(false);
   };
@@ -24,8 +23,15 @@ const MobileNav = () => {
 
   return (
     <div className="md:hidden">
-      <Menu isOpen={isOpen} onStateChange={handleStateChange} right styles={styles} customCrossIcon={ <img src="close.svg" alt="Close menu"/> }>
-        <span className="flex space-x-5">
+      <Menu 
+        isOpen={isOpen} 
+        onStateChange={handleStateChange} 
+        right 
+        styles={styles} 
+        customCrossIcon={<img src="/close.svg" alt="Close navigation menu"/>}
+        customBurgerIcon={<img src="/burger.svg" alt="Open navigation menu"/>}
+      >
+        <div className="flex space-x-5">
           <Link 
             href="https://artsolympiad.info/artwok_select.php"
             className="btn-primary mb-3 inline-block"
@@ -40,10 +46,15 @@ const MobileNav = () => {
           >
             Vote
           </Link>
-        </span>
+        </div>
         {links.map(link => (
-          <Link key={link.name} href={link.url} onClick={closeMenu} className="block py-1 text-white text-xl no-underline m:2">
-            <span className="hover:text-main-blue text-white">{link.name}</span>
+          <Link 
+            key={link.name} 
+            href={link.url} 
+            onClick={closeMenu} 
+            className="text-white text-lg py-2 hover:text-main-blue"
+          >
+            {link.name}
           </Link>
         ))}
       </Menu>
@@ -53,44 +64,26 @@ const MobileNav = () => {
 
 // Custom CSS
 const styles = {
-  bmBurgerButton: {
-    position: "fixed",
-    width: "36px",
-    height: "30px",
-    right: "36px",
-    top: "36px"
-  },
-  bmBurgerBars: {
-    background: "#373a47"
-  },
   bmMenuWrap: {
-    position: "fixed",
-    height: "50%",
-    width: "100%"
+    position: "absolute",
+    top: "8rem",
+    width: "100%",
+    height: "auto"
   },
   bmMenu: {
     background: "#1E1E1E",
-    padding: "2.5em 1.5em 0",
-    fontSize: "1.15em",
-    overflow: "auto"
+    padding: "5rem 2.5rem 1.5rem 2.5rem",
   },
-  bmMorphShape: {
-    fill: "#373a47"
-  },
-  bmItemList: {
-    color: "#b8b7ad",
-    padding: "0.8em",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    height: "100%"
-  },
-  bmItem: {
-    display: "block",
-    color: "white"
+  bmCrossButton: {
+    top: "2rem",
+    right: "2rem"
   },
   bmOverlay: {
-    background: "rgba(0, 0, 0, 0.3)"
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    background: "rgba(0, 0, 0, 0.3)",
+    zIndex: "-1",
   }
 };
 
