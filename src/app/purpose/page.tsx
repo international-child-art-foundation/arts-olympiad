49c582da2cb513e4dad88ae8d0c47838c70b18e6
Patrick Feliciano <96318160+pgf96@users.noreply.github.com>
2023-08-04 17:26:54 -0400

    Implement layout revisions on purpose page (#40)
    * feat: add spacing and equalize image sizes in Art Olympiad section
    
    * feat: change non-title text alignment from center to left
    
    * feat: update quote section to match revised figma design
    
    * style: adjust padding and margins on each content block for consistency
    
    * style: change spacing and text size on md breakpoints
    
    * style: change breakpoint of arts olympiad section from md to lg
    
    * style: adjust margins for tablet viewport
    
    * refactor: switch from custom width values to Tailwind's defined classes

--- src/app/purpose/page.tsx
+++ src/app/purpose/page.tsx
@@ -12,60 +12,64 @@ export default function page() {
       </div>
       {/* engaging creative kids block */}
       <div className="bg-secondary-blue pb-10">
-        <div className="bg-neutral-white rounded-2xl p-10 md:p-5 shadow-md flex flex-col md:flex-row items-start mx-auto w-11/12">
+        <div className="bg-neutral-white rounded-2xl px-10 py-10 lg:pb-16 md:p-5 shadow-md flex flex-col md:flex-row items-start mx-auto w-11/12">
           {/* earth part */}
-          <div className="flex flex-col items-center mb-15 md:w-3/7">
-            <h2 className="break-words font-semibold text-3xl text-center md:text-left pl-6 md:pl-10 pt-5 md:pt-10 mb-10">
-              Engaging Creative Kids in the Olympics
+          <div className="flex flex-col w-full items-center mb-15 md:w-2/5 md:mr-7 lg:mr-10">
+            <h2 className="font-semibold text-2xl lg:text-3xl text-center md:text-left md:pt-5 lg:pt-10">
+              Engaging Creative <br /> Kids in the Olympics
             </h2>
-            <Image className="w-3/5 h-auto" src="/purpose/icaf-2.svg" alt="An earth-like human waving at you with a smile" width={174} height={174} />
+            <Image className="mt-4 mb-2 w-auto" src="/purpose/icaf-2.svg" alt="An earth-like human waving at you with a smile" width={174} height={174} />
           </div>
           {/* pie chart part */}
-          <div className="flex flex-col items-center pt-0 md:pt-10 md:pl-20 md:w-4/7">
-            <div className="break-words text-base text-center md:text-left mb-5 md:mb-0">
+          <div className="flex flex-col justify-end items-center pt-0 md:pt-5 lg:pt-10 md:mr-5 lg:mr-10 md:w-3/5" >
+            <div className="break-words text-base text-left mb-5 md:mb-0">
               A survey of 9,000 kids by a U.K. advertising company, KidsKnowBest, revealed that about 50% seemed uninterested in watching the Olympics.
             </div>
-            <Image className="w-full h-auto md:mt-10 md:mr-10 hidden md:block" src="/purpose/chart-desktop.svg" alt="A pie chart shows 50% of children are not interested in Olympics" width={174} height={174} />
-            <Image className="w-full h-auto md:mt-10 md:mr-10 block md:hidden" src="/purpose/chart-mobile.svg" alt="A pie chart shows 50% of children are not interested in Olympics" width={174} height={174} />
+            <Image className="w-full md:mt-4 hidden md:block" src="/purpose/chart-desktop.svg" alt="A pie chart shows 50% of children are not interested in Olympics" width={174} height={174} />
+            <Image className="w-full h-auto max-w-72 block md:hidden" src="/purpose/chart-mobile.svg" alt="A pie chart shows 50% of children are not interested in Olympics" width={174} height={174} />
           </div>
         </div>
       </div>
       {/* Arts Olympiad block */}
-      <div className="bg-secondary-blue md:pt-10 pb-10">
-        <div className="bg-neutral-white rounded-2xl p-10 md:p-5 shadow-md flex flex-col items-start mx-auto w-11/12">
+      <div className="bg-secondary-blue lg:pt-10 pb-10">
+        <div className="bg-neutral-white rounded-2xl p-10 lg:p-5 shadow-md flex flex-col items-start mx-auto w-11/12">
           {/* skater part */}
-          <div className="flex flex flex-col md:flex-row items-left">
-            <div className="flex flex-col items-center md:w-2/5 md:mr-10">
-              <h2 className="break-words font-semibold text-3xl text-center md:text-left pt-5 md:pt-10 mb-10">
+          <div className="flex flex-col lg:flex-row items-left">
+            <div className="flex flex-col items-center lg:w-2/5 lg:mr-10">
+              <h2 className="break-words font-semibold text-2xl lg:text-3xl text-center lg:text-left lg:pt-10 ">
                 The Arts Olympiad
               </h2>
-              <Image className="w-1/2 h-1/2 md:mt-3" src="/purpose/illustration-08.svg" alt="A long hair skater with red sweater" width={211} height={211} />
+              <div className="flex items-center flex-col w-1/2 lg:w-5/12 h-1/2 my-5 lg:mt-10">
+                <Image src="/purpose/illustration-08.svg" alt="A long hair skater with red sweater" width={211} height={211}
+                  className="w-auto h-auto max-w-44 max-h-44"
+                />
+              </div>
             </div>
-            <div className="flex flex-col items-center md:w-3/5 md:mt-10 md:mr-10">
-              <div className="break-words text-base text-center md:text-left">
-                By bringing together countries and cultures from around the world, the Paris 2024 Olympics will reassure kids that their chaotic and divided world has hope. 
+            <div className="flex flex-col items-center lg:w-3/5 lg:mt-10 lg:mr-10">
+              <div className="break-words text-base text-left">
+                By bringing together countries and cultures from around the world, the Paris 2024 Olympics will reassure kids that their chaotic and divided world has hope.
               </div>
               <br></br>
-              <div className="break-words text-base text-center md:text-left">
+              <div className="break-words text-base text-left">
                 The Arts Olympiad brings excitement to Paris and a Create & Share interactivity that engages creative kids worldwide in the Olympics and the Olympic Movement. They participate in #MyFavoriteSport and become inspired by the Olympic values of excellence, friendship, and respect.
               </div>
               <br></br>
-              <div className="break-words text-base text-center md:text-left">
-                The Arts Olympiad connects Paris and Washington with the launch of the global contest at the Parisian exhibition in March 2024 and the announcement of the winners at the National Mall across the U.S. Capitol on July 1, 2024. 
+              <div className="break-words text-base text-left">
+                The Arts Olympiad connects Paris and Washington with the launch of the global contest at the Parisian exhibition in March 2024 and the announcement of the winners at the National Mall across the U.S. Capitol on July 1, 2024.
               </div>
             </div>
           </div>
           {/* water polo player part */}
-          <div className="flex flex flex-col md:flex-row items-left">
-            <div className="md:w-2/5 flex flex-col items-center">
-              <Image className="w-2/5 h-auto md:mt-10" src="/purpose/illustration-06.svg" alt="A smiling water polo player" width={211} height={211} />
+          <div className="flex flex flex-col lg:flex-row items-left">
+            <div className="lg:w-2/5 flex flex-col items-center mt-3 mb-3 lg:mt-14">
+              <Image className="w-auto h-auto max-w-44 max-h-44 " src="/purpose/illustration-06.svg" alt="A smiling water polo player" width={211} height={211} />
             </div>
-            <div className="flex flex-col items-center md:w-3/5 md:mt-10 md:mr-10 md:ml-10">
-              <div className="break-words text-base text-center md:text-left">
+            <div className="flex flex-col items-center lg:w-3/5 lg:mt-10 lg:mr-10 lg:ml-10">
+              <div className="break-words text-base text-left">
                 The global art contest on My Favorite Sport will bring personal and social benefits because creative kids face obesity risks in this TikTok sedentary era. Together, art and sport possess the power to inspire them to embrace the “Artist-Athlete Ideal” of the creative mind and healthy body (mens sana in corpore sano). When kids depict themselves as “artist-athletes” in their artwork on the theme, My Favorite Sport, their revised self-image as “artist-athletes” solidifies. Young artists and young athletes grow fellow feelings and empathic understanding, celebrating their creativity, diversity, and unity.
               </div>
               <br></br>
-              <div className="break-words text-base text-center md:text-left md:mb-5">
+              <div className="break-words text-base text-left lg:mb-5">
                 #MyFavoriteSport will go viral, involving millions of young artists worldwide to produce original works and young and old across the globe to vote for their favorite artwork.
               </div>
             </div>
@@ -75,67 +79,66 @@ export default function page() {
       </div>
       {/* ICAF & the Olympics block */}
       <div className="bg-neutral-white flex flex-col items-left pb-10">
-        <h2 className="break-words font-semibold text-3xl text-center md:text-left pl-6 md:ml-10 pt-10 mb-10">
+        <h2 className="font-semibold text-2xl lg:text-3xl text-center lg:text-left lg:pl-6 lg:ml-10 pt-10 mb-10">
           ICAF & the Olympics
         </h2>
-        <div className="flex flex-col md:flex-row ml-5 mr-5 md:ml-10 md:mr-10 items-start">
-          <div className="bg-main-blue rounded-2xl p-5 shadow-md flex flex-col items-center md:h-full md:ml-5 mb-5">
-            <div className="break-words text-base text-center text-white md:text-left">
-              Earlier Olympics, as visualized by Baron de Coubertin, had gold, silver, and bronze medals for bothathletes and artists. See the <Link href="https://www.smithsonianmag.com/arts-culture/when-the-olympics-gave-out-medals-for-art-6878965/" target="_blank" className="underline">Smithsonian Magazine </Link>. The International Child Art Foundation (ICAF) combines art and sport through the Arts Olympiad and #MyFavoriteSport.
+        <div className="flex flex-col lg:flex-row ml-5 mr-5 lg:ml-10 lg:mr-10 gap-3">
+          <div className="bg-main-blue rounded-2xl p-10 shadow-md items-center lg:ml-5 mb-5 lg:mb-0">
+            <div className="text-base text-white text-left">
+              Earlier Olympics, as visualized by Baron de Coubertin, had gold, silver, and bronze medals for bothathletes and artists. See the <Link href="https://www.smithsonianmag.com/arts-culture/when-the-olympics-gave-out-medals-for-art-6878965/" target="_blank" className="underline">Smithsonian Magazine.</Link> The International Child Art Foundation (ICAF) combines art and sport through the Arts Olympiad and #MyFavoriteSport.
             </div>
           </div>
-          <div className="bg-main-blue rounded-2xl p-5 shadow-md flex flex-col items-center md:h-full md:mr-10 md:ml-5">
-            <div className="break-words text-base text-center text-white md:text-left">
-              New York City’s bid to host the Olympics included the Arts Olympiad to engage young artists in the 2012 Olympics. ICAF has previously produced Arts Olympiad exhibitions at Olympic venues. Covid canceled the Arts Olympiad plans for the 2022 Japan and Beijing Olympics. The United States Olympic Committee has granted ICAF an exclusive license to use the “<Link href="/purpose/new-york-olympic.pdf" className="underline" target="_blank">Arts Olympiad</Link>” mark.
+          <div className="bg-main-blue rounded-2xl p-10 shadow-md items-center lg:mr-6">
+            <div className="text-base text-white text-left">
+              <Link href="/purpose/new-york-olympic.pdf" className="underline" target="_blank">New York City’s bid</Link> to host the Olympics included the Arts Olympiad to engage young artists in the 2012 Olympics. ICAF has previously produced Arts Olympiad exhibitions at Olympic venues. Covid canceled the Arts Olympiad plans for the 2022 Japan and Beijing Olympics. The United States Olympic Committee has granted ICAF an exclusive license to use the “Arts Olympiad” mark.
             </div>
           </div>
-          <div className="flex flex-col items-left mt-10 md:mt-0">
-            <div className="break-words text-base text-center md:text-left">
+          <div className="flex flex-col items-left ml-5 lg:ml-0 mt-10 lg:mt-10 min-w-72">
+            <div className="break-words text-base text-left ">
               To engage creative kids in the Olympics, please donate today
             </div>
             <br></br>
-            <Link href="https://icaf.org/donate.html" target="_blank" className="text-base text-left ml-5 inline-flex md:ml-0">
+            <Link href="https://icaf.org/donate.html" target="_blank" className="text-base text-left inline-flex">
               <u>https://icaf.org/donate.html</u>
-              <Image 
-                width = {23}
-                height = {23}
-                src="/about/link.svg" 
-                className="ml-2" 
-                alt="Icon denoting a hyperlink" 
+              <Image
+                width={23}
+                height={23}
+                src="/about/link.svg"
+                className="ml-2"
+                alt="Icon denoting a hyperlink"
               />
             </Link>
             <br></br>
-            <br></br>
-            <div className="break-words text-base text-left ml-5 md:ml-0">
+            <div className="break-words text-base text-left">
               For sponsorship opportunities, please <Link href="https://icaf.org/about/contact-us" target="_blank" className="underline">contact us</Link>
             </div>
           </div>
         </div>
       </div>
       {/* Nelson Mandela & Marcel Proust part    */}
-      <div className="bg-main-yellow flex flex-col md:flex-row items-center pb-10">
+      <div className="bg-main-yellow flex flex-col lg:flex-row items-center">
         {/* Nelson Mandela part */}
-        <div className="flex flex-row items-start p-10 md:w-1/2 md:pl-20">
-          <div className="flex flex-col items-center md:ml-20">
-            <Image className="w-full h-auto md:mt-10" src="/purpose/nelson.svg" alt="A smiling Nelson Mandela" width={211} height={211} />
-            <div className="break-words text-base text-center mb-5 mt-5">
+        <div className="flex flex-col lg:flex-row items-center p-10 lg:w-1/2 ">
+          <div className="flex flex-col items-center">
+            <Image className="w-full h-auto" src="/purpose/nelson.svg" alt="A smiling Nelson Mandela" width={211} height={211} />
+            <div className="break-words text-sm text-center mt-2.5 mb-3.5">
               Nelson Mandela
             </div>
           </div>
-          <div className="break-words text-base text-left ml-5 w-1/2 h-auto md:mt-10">
-            “ Sport has the power to change the world. It has the power to inspire. It has the power to unite people in a way that little else does. It speaks to youth in a language they understand. Sport can create hope where once there was only despair.”
+          <div className="break-words text-base text-left lg:ml-5 max-w-lg lg:w-1/2 self-start">
+            “Sport has the power to change the world. It has the power to inspire. It has the power to unite people in a way that little else does. It speaks to youth in a language they understand. Sport can create hope where once there was only despair.”
           </div>
         </div>
         {/* Marcel Proust part */}
-        <div className="flex flex-row items-start p-10 md:w-1/2 md:pl-20">
+        <div className="flex flex-col lg:flex-row items-center px-10 pb-10 lg:p-10 lg:w-1/2">
           <div className="flex flex-col items-center">
-            <Image className="w-full h-auto md:mt-10" src="/purpose/marcel.svg" alt="A photo of Marcel Proust" width={211} height={211} />
-            <div className="break-words text-base text-center mb-5 mt-5">
+            <Image className="w-full h-auto" src="/purpose/marcel.svg" alt="A photo of Marcel Proust" width={211} height={211} />
+            <div className="break-words text-sm text-center mt-2.5 mb-3.5">
               Marcel Proust
             </div>
           </div>
-          <div className="break-words text-base text-left ml-5 w-1/2 h-auto md:mt-10">
-            “ Through art alone are we able to emerge from ourselves to know what another person sees.”
+          <div className="break-words text-base text-left lg:ml-5 max-w-lg lg:w-1/2 h-auto self-start">
+            “Through art alone are we able to emerge from ourselves to know what another person sees.”
           </div>
         </div>
       </div>
