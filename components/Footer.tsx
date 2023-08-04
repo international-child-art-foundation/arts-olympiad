a421cd6c6839f243264f7b8f52d6e00864aa5117
Patrick Feliciano <96318160+pgf96@users.noreply.github.com>
2023-08-04 17:26:39 -0400

    Fix footer icon overflow and update logo (#39)
    * feat: add smaller ICAF logo to mobile
    
    * fix: correct icon overflow on mobile
    
    * refactor: combine img sources and remove unused wrappers and classes
    
    * chore: delete mobile ICAF logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import ICAF_logo from "../public/icaf-logo-transparent.svg";
+import ICAFLogo from "../public/icaf-logo-transparent.svg";
 import { InstagramIcon } from "./svgs/InstagramIcon";
 import { FacebookIcon } from "./svgs/FacebookIcon";
 import { TwitterIcon } from "./svgs/TwitterIcon";
@@ -17,30 +17,26 @@ const icons = [
   { SVGcomponent: TiktokIcon, altText:"Visit the International Child Art Foundation's Tiktok page", href:"https://www.tiktok.com/@internationalchildartfdn"},
   { SVGcomponent: LinkedinIcon, altText:"Visit the International Child Art Foundation's Linkedin page", href:"https://www.linkedin.com/company/international-child-art-foundation"},
   { SVGcomponent: PinterestIcon, altText:"Visit the International Child Art Foundation's Pinterest page", href:"https://www.pinterest.com/icaf/"},
-
-
 ];
+
 // Custom box shadows added to apply the effects of "shadow-md" with a negative offset-y
 const Footer = () => {
   return (
     <footer className="relative z-10 flex justify-center w-full shadow-[0_-4px_6px_-2px_rgba(0,0,0,0.1)] shadow-[0_-2px_4px_-2px_rgba(0,0,0,0.1)]">
       <div className="grid py-20 grid-col-1 lg:grid-cols-2 justify-center px-2 w-full max-w-7xl">
         <div className="flex justify-center items-center">
-          <div className="flex-initial w-fit pr-2 lg:pr-12">
-            <Image src={ICAF_logo} alt="ICAF logo"></Image>
-          </div>
-          <div className="w-fit">
-            <address className="font-normal text-sm md:font-medium lg:text-lg not-italic">
-              <Link href="http://www.icaf.org/" target="_blank" className="underline"> International Child Art Foundation </Link> <br />
-              2549 Virginia Avenue, NW <br />
-              Washington, DC 20037
-            </address>
-          </div>
+          <Image className="mr-2 lg:mr-12 hidden lg:block" width={163} src={ICAFLogo} alt="ICAF logo"/>
+          <Image className="mr-2 lg:mr-12 lg:hidden" width={111} src={ICAFLogo} alt="ICAF logo"/>
+          <address className="font-normal text-sm md:font-medium lg:text-lg not-italic">
+            <Link href="http://www.icaf.org/" target="_blank" className="underline"> International Child Art Foundation </Link> <br />
+            2549 Virginia Avenue, NW <br />
+            Washington, DC 20037
+          </address>
         </div>
         <div >
-          <ol className="flex justify-center items-center mt-9 lg:mt-0">
+          <ol className="flex justify-between lg:justify-center lg:space-x-3 mt-9 lg:mt-0">
             {icons.map((icon,i) => (
-              <li key={i} className="inline-block px-3">
+              <li key={i}>
                 <Link
                   aria-label={icon.altText}
                   href={icon.href}
