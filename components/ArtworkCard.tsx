84ea0b1ce9c04286ce7fba82cc01543b5d39f48a
RockyWangRC <Rocky-Wang@outlook.com>
2023-08-04 23:33:00 -0500

    feat:add back button and real artist info.

--- components/ArtworkCard.tsx
+++ components/ArtworkCard.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 type ArtworkCardProps = {
     id: string;
     name: string;
@@ -9,17 +8,12 @@ type ArtworkCardProps = {
 
 const ArtworkCard = ({ id, name, votes, url }: ArtworkCardProps) => {
   return (
-    <div id={id}>
-      <Link href={`/artwork/${id}`}>
-        <Image
-            src={url}
-            width={300}
-            height={350}
-            className="w-1/2 rounded-lg"
-            alt={name}
-          />
-        {/* <img className="w-4/8 rounded-lg" src={url} alt={name}/> */}
-      </Link>
+    <div id={id} className="relative w-full h-full">
+      <div className="w-full h-64 rounded-lg overflow-hidden">
+        <Link href={`/artwork/${id}`}>
+          <img className="w-full h-full" src={url} alt={name}/>
+        </Link>
+      </div>
       <p className="font-medium md:font-semi-bold text-lg">Name: {name}</p>
       <p className="font-normal md:font-semi-bold text-sm">Votes: {votes} </p>
     </div>
