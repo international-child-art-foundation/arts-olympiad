a082eb9dfbfdf8833e14964485d9ba8b44e5f273
RockyWangRC <Rocky-Wang@outlook.com>
2023-08-04 20:35:06 -0500

    feat:use updated artworks

--- components/ArtworkCard.tsx
+++ components/ArtworkCard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 type ArtworkCardProps = {
     id: string;
     name: string;
@@ -10,7 +11,14 @@ const ArtworkCard = ({ id, name, votes, url }: ArtworkCardProps) => {
   return (
     <div id={id}>
       <Link href={`/artwork/${id}`}>
-        <img className="w-4/8 rounded-lg" src={url} alt={name}/>
+        <Image
+            src={url}
+            width={300}
+            height={350}
+            className="w-1/2 rounded-lg"
+            alt={name}
+          />
+        {/* <img className="w-4/8 rounded-lg" src={url} alt={name}/> */}
       </Link>
       <p className="font-medium md:font-semi-bold text-lg">Name: {name}</p>
       <p className="font-normal md:font-semi-bold text-sm">Votes: {votes} </p>
