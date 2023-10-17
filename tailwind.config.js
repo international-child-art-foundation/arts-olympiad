fdcadab8ee3779f47adb759242042fe005e52239
Jiayun Yan <52384601+yanjiayun@users.noreply.github.com>
2023-10-17 15:18:46 -0400

    feat: footer for phase 2 (#57)
    fix: button overlap when text is too long
    fix: placement of social media icons
    fix: margin/vertical gap/max-width/text center
    fix: font/button style

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,34 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
+  theme: {                                                       
+    screens:{
+      'sm': '320px',
+      'md': '481px',
+      'lg': '769px',
+      'xl': '1025px',
+    },
     extend: {
       colors:{
         'secondary-blue': '#98CEFF',
         'main-blue': '#3256D5',
+        'new-blue': '#134380',
         'neutral-black': '#1F1F23',
         'neutral-white': '#F9FAF6',
         'main-yellow': '#FCE983',
         'main-gold': '#E7A004',
         'main-silver': '#919191',
         'main-bronze': '#AD8122',
+        'main-red': '#FF0000',
+        'main-grey':'#6A7682',
+      },
+      fontFamily:{
+        body: ['Open Sans']
+      },
+      spacing:{
+        '5%': '5%',
+        '10%': '10%',
+        '30%': '30%',
       },
       fontSize: {
         "heading-1": ["2.125rem",{
