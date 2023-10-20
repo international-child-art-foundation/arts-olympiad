3f9ce858331ca7f5d43e580fb82b79756e6df74c
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2023-10-20 14:47:18 -0400

    chore: Update all packages, implement svg imports

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,6 +6,8 @@ import { TwitterIcon } from "./svgs/TwitterIcon";
 import { YoutubeIcon } from "./svgs/YoutubeIcon";
 import { LinkedinIcon } from "./svgs/LinkedinIcon";
 import { PinterestIcon } from "./svgs/PinterestIcon";
+import { EmailIcon } from "./svgs/EmailIcon";
+import { HeartIcon } from "./svgs/HeartIcon";
 
 const icons = [
   { SVGcomponent: FacebookIcon, altText:"Visit the International Child Art Foundation's Facebook page", href:"https://www.facebook.com/ICAF.org"},
@@ -33,7 +35,7 @@ const Footer = () => {
                 autoComplete="off"
                 className="h-10 min-w-min	flex-1 flex-grow w-full tracking-wide md:w-auto pl-10 pr-3 border border-main-grey rounded-md required">  
               </input>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="absolute pointer-events-none w-5 h-5 ml-3 mt-2.5"><path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
+              <EmailIcon />
             </div>
             <a href="#" className="h-fit md:flex-1 border-neutral-white whitespace-nowrap border rounded text-center py-2 px-3 text-sm cursor-pointer tracking-wide w-full md:w-auto text-neutral-white">Sign up</a>
           </div>
@@ -71,7 +73,10 @@ const Footer = () => {
         <div className="flex flex-wrap lg:justify-center lg:gap-9 sm:w-full lg:order-4 xl:w-auto xl:flex-col xl:gap-5"> 
           <div className="w-1/2 lg:w-auto">
             <div className="flex flex-col w-auto sm:items-left lg:items-center">
-              <a href="#" className="group w-32 xl:w-32 h-fit border-neutral-white border rounded text-center py-3 px-5 text-sm cursor-pointer tracking-wide bg-neutral-white text-new-blue"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#134380" className="w-4 h-4 inline-block mr-1 mb-1 group-hover:fill-main-red group-hover:stroke-main-red"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>Donate</a>
+              <a href="#" className="group w-32 xl:w-32 h-fit border-neutral-white border rounded text-center py-3 px-5 text-sm cursor-pointer tracking-wide bg-neutral-white text-new-blue">
+                <HeartIcon />
+                Donate
+              </a>
             </div>
           </div>
           <div className="w-1/2 lg:w-auto">
