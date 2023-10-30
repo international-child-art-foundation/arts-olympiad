f7bffcc5b64b1dc02d378f9e55f937af6324d442
Jiayun Yan <52384601+yanjiayun@users.noreply.github.com>
2023-10-30 17:48:25 -0400

    feat: header for Phase 2

---
+++ components/svgs/DownIcon.tsx
@@ -0,0 +1,5 @@
+export const DownIcon = () => (
+  <svg className="w-2.5 h-2.5 ml-1.5 mt-1 md:mt-1.5 group-hover:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m1 1 4 4 4-4" />
+  </svg>
+);
