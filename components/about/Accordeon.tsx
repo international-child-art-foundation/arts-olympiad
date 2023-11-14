eb5ba59954b3b76cc201002449ca4e805ba9f7b4
akatsay <akatsay2@gmail.com>
2023-11-14 14:06:32 -0500

    feat: animate accordeon

--- components/about/Accordeon.tsx
+++ components/about/Accordeon.tsx
@@ -3,6 +3,9 @@ import React from "next/image";
 import {AccordeonCard} from "./AccordeonCard";
 import {useState} from "react";
 import {Pm} from "../common/texts/Pm";
+import {artworks} from "../../mock/artworks";
+import {LazyImage} from "../common/images/LazyImage";
+import {AboutArtworkCard} from "./AboutArtworkCard";
 
 export const Accordeon = () => {
 
@@ -21,9 +24,9 @@ export const Accordeon = () => {
       {/*  </H2m>*/}
       {/*</article>*/}
     
-      <div className="flex flex-row rounded-xl">
+      <div className="flex flex-col lg:flex-row">
         <AccordeonCard
-          className="rounded-l-xl"
+          className="rounded-t-xl lg:rounded-l-xl lg:rounded-tr-none"
           isOpen={cardOpen === 1}
           setIsOpen={(i) => setCardOpen(i)}
           color="#E4F9EA"
@@ -31,12 +34,18 @@ export const Accordeon = () => {
           header="Why #MyFavoriteSport?"
           paragraph={
             <>
-              <Pm className="my-12">We understand the profound impact that artistic expression can have on a child's development.</Pm>
+              <Pm className="lg:mt-12">We understand the profound impact that artistic expression can have on a child's development.</Pm>
               <Pm className="my-12">#MyFavoriteSport encourages students to embrace what we call the "Artist-Athlete Ideal" — the idea that a creative mind and a healthy body go hand in hand (mens sana in corpore sano).</Pm>
-              <Pm className="my-12">By linking imagination with embodiment, this program solidifies a student's self-image as an artist-athlete, celebrating both their artistic talents and physical well-being.</Pm>
+              <Pm className="mb-12">By linking imagination with embodiment, this program solidifies a student's self-image as an artist-athlete, celebrating both their artistic talents and physical well-being.</Pm>
             </>
           }
-          images={<></>}
+          images={
+            <div className="flex flex-col md:flex-row mx-auto lg:flex-col xl:flex-row xl:mx-0">
+              <AboutArtworkCard artwork={artworks[12]}/>
+              <div className="my-4 md:my-0 lg:my-4 xl:my-0 mx-4" />
+              <AboutArtworkCard artwork={artworks[13]}/>
+            </div>
+          }
         />
         <AccordeonCard
           isOpen={cardOpen === 2}
@@ -44,8 +53,14 @@ export const Accordeon = () => {
           color="#FFF5AD"
           number={2}
           header="Inspired by the Vision of Baron de Coubertin"
-          paragraph="Baron de Coubertin, the visionary behind the modern Olympics, believed in showcasing humanity's physical prowess and artistic talents. #MyFavoriteSport art contest revives this vision, inviting young artists to express themselves through their favorite sport, celebrating the intersection of art and athleticism."
-          images={<></>}
+          paragraph={<Pm className="mb-12 lg:my-12">Baron de Coubertin, the visionary behind the modern Olympics, believed in showcasing humanity's physical prowess and artistic talents. #MyFavoriteSport art contest revives this vision, inviting young artists to express themselves through their favorite sport, celebrating the intersection of art and athleticism."</Pm>}
+          images={
+            <div className="flex flex-col md:flex-row mx-auto lg:mx-0">
+              <LazyImage className="min-w-[230px] min-h-[300px] max-w-[230px] max-h-[300px]" imageUrl="/about/olympic-monument.webp" alt="olympic-monument." />
+              <div className="my-4 md:my-0 mx-4" />
+              <LazyImage className="min-w-[230px] min-h-[300px] max-w-[230px] max-h-[300px]" imageUrl="/about/baron-de-couberin.jfif" alt="baron-de-couberin." />
+            </div>
+          }
         />
         <AccordeonCard
           isOpen={cardOpen === 3}
@@ -53,18 +68,22 @@ export const Accordeon = () => {
           color="#CCEBFF"
           number={3}
           header="Recognized by the U.S. Olympic Committee"
-          paragraph="The U.S. Olympic and Paralympic Committee has granted ICAF an exclusive license to use the 'Arts Olympiad' mark for #MyFavoriteSport."
-          images={<></>}
+          paragraph={<Pm className="mb-12 lg:my-12">The U.S. Olympic and Paralympic Committee has granted ICAF an exclusive license to use the 'Arts Olympiad' mark for #MyFavoriteSport."</Pm>}
+          images={
+            <LazyImage className="max-w-[500px] max-h-[315px]" imageUrl="/svgs/icao-logo.svg" alt="Internation Child Art Olympiad logo." />
+          }
         />
         <AccordeonCard
-          className="rounded-r-xl"
+          className="rounded-b-xl lg:rounded-r-xl lg:rounded-bl-none"
           isOpen={cardOpen === 4}
           setIsOpen={(i) => setCardOpen(i)}
           color="#F9E4EE"
           number={4}
           header="More about ICAF"
-          paragraph="ICAF is more than just #MyFavoriteSport. We are proud to be ranked among the 25 Top Children’s Charities in the United States. Beyond #MyFavoriteSport, ICAF also organizes Healing Art Programs to revive faith in the natural world for child victims of natural disasters and Peace through Art Programs to restore trust in humanity for children in conflict zones."
-          images={<></>}
+          paragraph={<Pm className="mb-12 lg:my-12">ICAF is more than just #MyFavoriteSport. We are proud to be ranked among the 25 Top Children’s Charities in the United States. Beyond #MyFavoriteSport, ICAF also organizes Healing Art Programs to revive faith in the natural world for child victims of natural disasters and Peace through Art Programs to restore trust in humanity for children in conflict zones.</Pm>}
+          images={
+            <LazyImage className="max-w-[500px] max-h-[315px]" imageUrl="/svgs/Icaf-logo.svg" alt="Internation Child Art Olympiad logo." />
+          }
         />
       </div>
 
--- components/about/AccordeonCard.tsx
+++ components/about/AccordeonCard.tsx
@@ -1,7 +1,8 @@
 import {ReactNode} from "react";
 import {H3m} from "../common/texts/H3m";
-import {Pm} from "../common/texts/Pm";
 import {H2m} from "../common/texts/H2m";
+import useWindowDimensions from "@/hooks/useWindowDimensions";
+import "../../src/styles/accordeon.css";
 
 interface IProps {
   className?: string
@@ -10,31 +11,50 @@ interface IProps {
   color: string
   number: number
   header: string
-  paragraph: string | ReactNode
+  paragraph: ReactNode
   images: ReactNode
 }
 
 export const AccordeonCard = ({className, isOpen, setIsOpen, color, number, header, paragraph, images}: IProps) => {
 
+  const {windowWidth} = useWindowDimensions();
+  const displayhorizontally = windowWidth >= 1024;
+
   return (
     <article
       style={{backgroundColor: color}}
-      className={`${className} flex flex-row justify-between min-h-[650px] min-w-[80px] cursor-pointer`}
+      className={`
+      ${
+    isOpen && displayhorizontally ? "slide-in" : !isOpen && displayhorizontally ? "slide-out" :
+      isOpen && !displayhorizontally ? "slide-up" : !isOpen && !displayhorizontally && "slide-down"
+    }
+      ${className} flex flex-col 
+      min-h-[85px]
+      lg:flex-row justify-between  lg:min-h-[650px] lg:min-w-[85px] 
+      cursor-pointer
+      `}
       onClick={() => setIsOpen(number)}
     >
-      <div className="flex flex-col justify-between min-w-[80px] p-6">
-        <Pm className="font-bold text-center">0{number}</Pm>
-        <H3m style={{writingMode: "vertical-lr", transform: "rotate(180deg)"}}>{header}</H3m>
+      <div className="flex flex-row lg:flex-col lg:justify-between min-w-[80px] p-6">
+        <H3m className="font-bold text-center mr-6 lg:mr-0">0{number}</H3m>
+        <H3m
+          className={!displayhorizontally ? "font-semibold" : ""}
+          style={{writingMode: displayhorizontally ? "vertical-lr": "horizontal-tb", transform: displayhorizontally ? "rotate(180deg)" : ""}}
+        >
+          {header}
+        </H3m>
       </div>
 
-      {
-        isOpen &&
-        <div className="p-12">
-          <H2m>{header}</H2m>
-          <Pm className="my-12">{paragraph}</Pm>
-          {images}
-        </div>
-      }
+      <div
+        className={`${isOpen ? "content-in" : "content-out"} flex flex-col justify-between p-12`}
+      >
+        {
+          displayhorizontally &&
+            <H2m>{header}</H2m>
+        }
+        {paragraph}
+        {images}
+      </div>
 
     </article>
   );
