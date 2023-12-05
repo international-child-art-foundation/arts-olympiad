7c47a36f79ea04ecb1dcc9fe8d2098d91d4bd87e
akatsay <akatsay2@gmail.com>
2023-12-04 23:12:27 -0500

    feat: start working on contact form

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,22 +1,22 @@
 "use client";
 import {useState} from "react";
-import {ContactForm} from "./ContactForm";
+import {ContactFormBox} from "./ContactFormBox";
 import {ContactConfirmation} from "./ContactConfirmation";
 import Image from "next/image";
 
 export const Contact = () => {
 
-  const [isSubmitted, setIsSubmitted] = useState(true);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   return (
     <section
       className="self-center my-8 mx-4 lg:mx-12 xl:mx-20 md:my-20 xl:my-32 relative max-w-screen-2xl m-auto"
     >
-      <Image src="/contact/yellow-outlined-blob.svg" alt="" width={452} height={1} className="absolute -top-28 -left-48 " />
-      <Image src="/contact/blue-outlined-blob.svg" alt="" width={447} height={1} className="absolute -bottom-28 -right-48" />
+      <Image src="/contact/yellow-outlined-blob.svg" alt="" width={452} height={1} className="absolute -top-28 -left-48 -z-10 " />
+      <Image src="/contact/blue-outlined-blob.svg" alt="" width={447} height={1} className="absolute -bottom-28 -right-48 -z-10" />
       {
         !isSubmitted ?
-          <ContactForm setIsSubmitted={setIsSubmitted}/>
+          <ContactFormBox setIsSubmitted={setIsSubmitted}/>
           :
           <ContactConfirmation />
       }
--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,11 +1,76 @@
-import {Dispatch, SetStateAction} from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-interface IProps {
-  setIsSubmitted: Dispatch<SetStateAction<boolean>>
+type Query = "Issue submitting artwork" | "Someone uploaded my work" | "Trouble sharing artwork" |
+  "Voting assistance" | "Account issues" | "Other" | null
+
+interface IFormValues {
+  firstName: string,
+  lastName: string,
+  phone: string,
+  email: string,
+  query: Query
 }
 
-export const ContactForm = ({ setIsSubmitted } : IProps) => {
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required("First Name is required"),
+  lastName: Yup.string().required("Last Name is required"),
+  email: Yup.string().email("Invalid email address").required("Email is required"),
+  phone: Yup.string().required("Phone Number is required"),
+});
+
+const initialValues: IFormValues = {
+  firstName: "",
+  lastName: "",
+  phone: "",
+  email: "",
+  query: null
+};
+
+const onSubmit = (values: IFormValues) => {
+  // Handle form submission logic here
+  console.log(values);
+};
+
+export const ContactForm = () => {
   return (
-    <></>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form className="grid grid-rows-6 grid-cols-2">
+        <div className="row-span-2 col-span-2">
+          <div className="col-span-1 row-span-1" >
+            <div className="w-full">
+              <label htmlFor="firstName">First Name</label>
+              <Field type="text" id="firstName" name="firstName" />
+              <ErrorMessage name="firstName" component="div" />
+            </div>
+
+            <div>
+              <label htmlFor="lastName">Last Name</label>
+              <Field type="text" id="lastName" name="lastName" />
+              <ErrorMessage name="lastName" component="div" />
+            </div>
+          </div>
+
+          <div className="col-span-1 row-span-1" >
+            <div>
+              <label htmlFor="email">Email</label>
+              <Field type="text" id="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+            </div>
+
+            <div>
+              <label htmlFor="phone">Phone Number</label>
+              <Field type="text" id="phone" name="phone" />
+              <ErrorMessage name="phone" component="div" />
+            </div>
+          </div>
+        </div>
+
+      </Form>
+    </Formik>
   );
 };
