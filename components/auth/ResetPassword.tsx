6df29e312d3fb2715bcccbf93e6d7159931ec462
akatsay <akatsay2@gmail.com>
2024-03-05 10:15:17 -0500

    chore: add blobs to reset page and fix minimal height for the form

--- components/auth/ResetPassword.tsx
+++ components/auth/ResetPassword.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-// import Image from "next/image";
+import Image from "next/image";
 import {useState} from "react";
 import {SendResetLinkForm} from "./SendResetLinkForm";
 import {ResetPasswordForm} from "./ResetPasswordForm";
 import {ResetPasswordSuccess} from "./ResetPasswordSuccess";
+import blueBigBlob from "../../public/auth/blobs/blue_big_blob.svg";
+import pinkOutlinedBlob from "../../public/auth/blobs/pink_outlined_blob.svg";
+import yellowOutlinedBlob from "../../public/auth/blobs/yellow_outlined_blob.svg";
 
 export type TFormStatus = "send-link" | "link-sent" | "reset-form" | "success";
 
@@ -23,7 +26,7 @@ export const ResetPassword = () => {
     <>
       <div className="relative">
         <section
-          className="min-h-[40vh] w-full my-20 mb-24 sm:px-8 md:px-12 lg:px-16 xl:px-20 relative max-w-screen-2xl m-auto"
+          className="min-h-[650px] w-full my-20 mb-24 sm:px-8 md:px-12 lg:px-16 xl:px-20 relative max-w-screen-2xl m-auto"
         >
           <div className="h-full flex flex-row justify-center items-center">
             <div className="my-auto max-w-[90%] sm:max-w-[60%] lg:max-w-[40%]">
@@ -40,19 +43,14 @@ export const ResetPassword = () => {
         </section>
 
         {/*  /!*Desktop blobs*!/*/}
-        {/*  <Image src={YellowBlob} alt="" width={712} className="hidden lg:block absolute -z-10 -left-64 -bottom-80" />*/}
-        {/*  <Image src={PinkBlob} alt="" width={400} className="hidden lg:block absolute -z-10 left-72 bottom-24" />*/}
-        {/*  <Image src={BlueSmallBlob} alt="" width={298} className="hidden lg:block absolute -z-10 -right-32 -bottom-20" />*/}
+        <Image src={blueBigBlob} alt="" width={800} className="hidden lg:block absolute -z-10 -left-64 -bottom-60" />
+        <Image src={yellowOutlinedBlob} alt="" width={400} className="hidden lg:block absolute -z-20 -left-40 bottom-64" />
+        <Image src={pinkOutlinedBlob} alt="" width={750} className="hidden lg:block absolute -z-10 -right-80 -bottom-80" />
         {/*  /!*Mobile blobs*!/*/}
-        {/*  <Image src={PinkMobileBlob} alt="" width={426} className="xsm:hidden absolute -z-10 -right-48 top-32" />*/}
-        {/*  <Image src={BlueSmallBlob} alt="" width={298} className="lg:hidden absolute -z-10 -right-32 -bottom-0" />*/}
+        <Image src={yellowOutlinedBlob} alt="" width={500} className="block lg:hidden absolute -z-10 -left-72 -top-72" />
+        <Image src={pinkOutlinedBlob} alt="" width={350} className="block lg:hidden absolute -z-10 -right-32 -bottom-40" />
       </div>
-
-
       {/*/!*Overheader blobs*!/*/}
-      {/*<Image src={BlueBlob} alt="" width={ windowWidth >= 1280 ? 760 : 680} className="hidden lg:block absolute -z-10 left-0 -top-20" />*/}
-      {/*<Image src={GreenBlob} alt="" width={264} className="hidden lg:block absolute -z-10 -left-32 top-80" />*/}
-      {/*<Image src={GreenBlob} alt="" width={windowWidth >= 640 ? 285 : 184} className="lg:hidden absolute -z-10 -left-24 -top-0" />*/}
     </>
   );
 };
