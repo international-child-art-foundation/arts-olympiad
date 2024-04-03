80cc10603deaab023ae322f054b3b9a5625a9d2a
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2024-04-03 18:00:34 -0400

    fix: remedy uncentered tag text

--- components/gallery/Tag.tsx
+++ components/gallery/Tag.tsx
@@ -8,7 +8,7 @@ interface TagProps {
 
 export const Tag: React.FC<TagProps> = ({ label, filterType, onRemove }) => {
   return (
-    <div className="bg-new-blue text-white px-4 py-2 rounded cursor-pointer h-[40px] select-none" onClick={() => onRemove(filterType, label)}>
+    <div className="bg-new-blue text-white px-4 py-2 rounded cursor-pointer h-[40px] select-none content-center" onClick={() => onRemove(filterType, label)}>
       {label}
       <span className="ml-2">✕</span>
     </div>
