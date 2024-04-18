0d63c493edf48970c8db68a27d10a2566a98f62a
yanjiayun <Anyayan168@gmail.com>
2024-04-18 16:12:00 -0400

    feat:upload image progress bar

--- components/auth/modal/CustomUploadImage.tsx
+++ components/auth/modal/CustomUploadImage.tsx
@@ -1,6 +1,13 @@
 import { useField } from "formik";
 import { UploadIcon } from "../../svgs/UploadIcon";
 import { Field } from "formik";
+import { useState } from "react";
+import { Loader0 } from "../../../public/auth/modal/Loader0";
+import { Loader9 } from "../../../public/auth/modal/Loader9";
+import { Loader33 } from "../../../public/auth/modal/Loader33";
+import { Loader63 } from "../../../public/auth/modal/Loader63";
+import { Loader87 } from "../../../public/auth/modal/Loader87";
+import { Loader100 } from "../../../public/auth/modal/Loader100";
 
 interface CustomUploadImageProps {
   label: string;
@@ -11,9 +18,28 @@ interface CustomUploadImageProps {
 
 export const CustomUploadImage = ({label, ...props} : CustomUploadImageProps) => {
   const [field, meta, helpers] = useField(props);
+  const [uploadProgress, setUploadProgress] = useState(-1);
+
+  const simulateUploadProgress = () => {
+    const progressValues = [0, 9, 33, 63, 87, 100];
+    let currentProgressIndex = 0;
+
+    const updateProgress = () => {
+      setUploadProgress(progressValues[currentProgressIndex]);
+      currentProgressIndex++;
+
+      if (currentProgressIndex < progressValues.length) {
+        setTimeout(updateProgress, 500); 
+      }
+    };
+
+    updateProgress();
+  };
+
   return(
     <>
       <label htmlFor="image" className="w-full h-64 mb-6 border border-neutral-black rounded-lg pl-4 pr-4 pt-2 pb-2 flex flex-col items-center justify-center">
+        {uploadProgress === -1 &&        
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <p className="mb-6 text-md font-light text-neutral-black">{label}</p>
           <div className="mb-6 h-fit w-fit rounded text-center py-4 px-6 text-base font-normal bg-new-blue text-neutral-white">
@@ -21,7 +47,7 @@ export const CustomUploadImage = ({label, ...props} : CustomUploadImageProps) =>
             <span className="ml-4">Upload Artwork</span>
           </div>
           <p className="text-sm font-light text-neutral-black">PNG or JPG</p>
-        </div>
+        </div>}
         <Field 
           {...field} 
           {...props} 
@@ -35,9 +61,24 @@ export const CustomUploadImage = ({label, ...props} : CustomUploadImageProps) =>
             const file = event.currentTarget.files && event.currentTarget.files[0];
             if (file) {
               helpers.setValue(file);
-            }
+              if(!meta.error){
+                simulateUploadProgress();
+              }
+            };
           }}
         />
+        <div className="flex ">
+          <div className="w-full m-auto">
+            {uploadProgress === 0 && <Loader0 />}
+            {uploadProgress === 9 && <Loader9 />}
+            {uploadProgress === 33 && <Loader33 />}
+            {uploadProgress === 63 && <Loader63 />}
+            {uploadProgress === 87 && <Loader87 />}
+            {uploadProgress === 100 && <Loader100 />}
+            {uploadProgress !== -1 && uploadProgress !== 100 && <p className="text-sm font-light mt-4">Your artwork is uploading...</p>}
+            {uploadProgress === 100 && <p className="text-sm font-light mt-4">Your artwork is uploaded!</p>}
+          </div>
+        </div>
       </label>
       {meta.error &&
         <div className="inline-flex mt-1 bg-[#FBF4F3] w-full rounded border-l-8 border-[#EE2F4D]"> 
