46c8f27c4f7c60172af276c9cc382253790eab3a
Hayden Linder <haydenlinder@gmail.com>
2024-05-06 17:15:15 -0700

    Edit command

--- components/common/form_inputs/TextInput.tsx
+++ components/common/form_inputs/TextInput.tsx
@@ -1,53 +1,71 @@
 import React from "react";
-import { Field} from "formik";
-import {InputLabel} from "./InputLabel";
-import {ErrorSuccessPopUp} from "./ErrorSuccessPopUp";
+import { Field } from "formik";
+import { InputLabel } from "./InputLabel";
+import { ErrorSuccessPopUp } from "./ErrorSuccessPopUp";
 
 interface IProps {
-  className?: string
-  labelText: string
-  id: string
-  placeholder?: string
-  inputType?: string
-  error: string | undefined
-  touched: boolean | undefined
-  value: string | undefined
-  required?: boolean
+  className?: string;
+  labelText: string;
+  id: string;
+  placeholder?: string;
+  inputType?: string;
+  error: string | undefined;
+  touched: boolean | undefined;
+  value: string | undefined;
+  required?: boolean;
 }
-export const TextInput = ({className, labelText, id, placeholder, inputType, error, touched, value, required = true}: IProps) => {
-// test change to format staged file
+export const TextInput = ({
+  className,
+  labelText,
+  id,
+  placeholder,
+  inputType,
+  error,
+  touched,
+  value,
+  required = true,
+}: IProps) => {
+  // test change to format staged file
   return (
-    <div className={`${className} my-1 flex flex-col`} >
+    <div className={`${className} my-1 flex flex-col`}>
       <InputLabel
         className={`font-semibold ${
-          required && error && touched ? "text-accent-red"
-            :
-            required &&!error && touched && value ?  "text-accent-green"
-              :
-              ""
+          required && error && touched
+            ? "text-accent-red"
+            : required && !error && touched && value
+              ? "text-accent-green"
+              : ""
         }`}
-        labelText={labelText} htmlFor={id} error={error} touched={touched}
+        labelText={labelText}
+        htmlFor={id}
+        error={error}
+        touched={touched}
       />
       <Field
         className={`
           my-2 p-3 border-1 rounded-lg 
           ${
-    required && error && touched ? "border-accent-red placeholder:text-accent-red"  
-      :
-      required &&!error && touched && value ? "border-accent-green" 
-        :
-        "border-black"
-    }
+            required && error && touched
+              ? "border-accent-red placeholder:text-accent-red"
+              : required && !error && touched && value
+                ? "border-accent-green"
+                : "border-black"
+          }
 
         `}
-        placeholder={ placeholder || "Start typing here..."}
-        type={inputType || "text" }
+        placeholder={placeholder || "Start typing here..."}
+        type={inputType || "text"}
         id={id}
         name={id}
-      >
-      </Field>
+      ></Field>
 
-      <ErrorSuccessPopUp required={required} id={id} error={error} touched={touched} value={value} />
+      <ErrorSuccessPopUp
+        required={required}
+        id={id}
+        error={error}
+        touched={touched}
+        value={value}
+      />
     </div>
   );
 };
