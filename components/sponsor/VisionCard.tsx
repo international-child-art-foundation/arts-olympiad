47bd466b831e91ad5f52fc53673a4b45b352eb22
Jiren1247 <jiren@vt.edu>
2024-05-16 21:54:58 -0400

    feat:sponsor page p2 update

--- components/sponsor/VisionCard.tsx
+++ components/sponsor/VisionCard.tsx
@@ -2,7 +2,6 @@
 import "../../src/styles/fade-in-out-texture.css";
 import React, {StaticImageData} from "next/image";
 import {ReactNode, useState} from "react";
-import {Pm} from "../common/texts/Pm";
 import Image from "next/image";
 import useWindowDimensions from "@/hooks/useWindowDimensions";
 
@@ -22,8 +21,8 @@ export const VisionCard = ({ icon, heading, description, color, texture, ...rest
 
   return (
     <article
-      className={`relative lg:max-w-[30%] lg:min-h-[440px] flex flex-col justify-stretch items-center p-6 mb-6 rounded-xl border-1 ${restProps.className}`}
-      style={{boxShadow: "5px 6px 25px 4px rgba(0, 0, 0, 0.18)", backgroundColor: color}}
+      className={`relative lg:w-1/4 flex flex-col justify-stretch items-center p-6 mb-6 rounded-xl border-1 ${restProps.className}`}
+      style={{backgroundColor: color}}
       onMouseOver={() => !touchScreenPrimary && setRevealTexture(true)}
       onMouseOut={() => !touchScreenPrimary && setRevealTexture(false)}
       onTouchStart={() => touchScreenPrimary && setRevealTexture(!revealTexture)}
@@ -31,13 +30,13 @@ export const VisionCard = ({ icon, heading, description, color, texture, ...rest
     >
       <div className="w-full mt-10 flex flex-row xsm:flex-col justify-start items-center">
         {/* <Image src={icon} alt="" width={isTablet ? 40 : 20} height={20} className={`${!isTablet && "mr-4"}`}/> */}
-        <Image src={icon} alt="" width={isTablet ? 40 : 20} height={20} className="select-none pointer-events-none"/>
-        <h3 className="font-medium lg:font-semibold text-3xl my-5 font-montserrat">{heading}</h3>
+        <Image src={icon} alt="" width={isTablet ? 60 : 60} height={20} className="select-none pointer-events-none"/>
+        <h3 className="font-semibold lg:font-semibold text-2xl my-5 font-montserrat text-center">{heading}</h3>
       </div>
-      {typeof(description) == "string" ? <Pm className="font-sans font-light text-base mb-6">{description}</Pm> : description}
+      {typeof(description) == "string" ? <p className="font-sans font-light text-base mb-6 text-3xl w-full md:w-2/3 lg:w-full">{description}</p> : description}
       {
         isTablet &&
-        <div className={`${revealTexture ? "opacity-1" : "opacity-0" }`}>
+        <div className={`${revealTexture ? "opacity-50" : "opacity-0" }`}>
           {texture}
         </div>
       }
