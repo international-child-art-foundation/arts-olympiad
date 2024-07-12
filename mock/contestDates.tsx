1d9ffa37f5aba1b28d343611ca8bb5626fd85117
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2024-07-12 15:19:14 -0400

    fix: infinite render bug (#195)

--- mock/contestDates.tsx
+++
@@ -1,9 +0,0 @@
-import dates from "./dates";
-
-const contestStartTime = new Date(dates.competitionBegin);
-contestStartTime.setHours(12, 0, 0);
-
-const contestEndTime = new Date(dates.competitionEnd);
-contestEndTime.setHours(23, 59, 59);
-
-export { contestStartTime, contestEndTime };
