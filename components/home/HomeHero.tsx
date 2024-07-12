06eb716f3d12b12c72271fd14bd234e01cab75d6
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2024-07-12 14:21:21 -0400

    Emma P3 landing page edits (#192)
    * feat: change the details on about page
    
    * feat: p3 landing page
    
    * fix
    
    * fix: bugs free
    
    * fix: hero, getInvolved, Wisdom bugs free
    
    * fix: hero contexts changed
    
    * fix: hero texts change, hero & wisdom bugs free
    
    * fix: change the rainbowline banner
    
    * feat: change the sponsor logo to transparent background
    
    ---------
    
    Co-authored-by: Jiren1247 <jiren@vt.edu>
    Co-authored-by: Jiren Wang <143107647+Jiren1247@users.noreply.github.com>

--- components/home/HomeHero.tsx
+++ components/home/HomeHero.tsx
@@ -3,54 +3,60 @@ import Image from "next/image";
 import hashtag from "../../public/home/hashtag.svg";
 // import bicycleHero from "../../public/home/MFS_Bicycle_Hero.webp";
 import HeroHeader from "../../public/home/Hero_Header.jpg";
-import { formatDate }  from "../../mock/dates";
+import { formatDate } from "../../mock/dates";
+import ranbowLine from "../../public/home/rainbowLine.png";
+// import RainbowLine from "../../public/home/new-hero/RainbowLine.svg";
 
 export const Intro = () => {
 
   return (
-    <section aria-label="introduction." className=" gap-8 mb-36 md:mb-28 mt-6 md:mt-20 relative bg-neutral-white lg:grid lg:grid-rows-1 col-span-8 lg:col-span-12 lg:grid-cols-12 px-8 md:px-12 lg:px-16 xl:px-20 max-w-screen-2xl m-auto z-0 ">
+    <>
+      <section aria-label="introduction." className="gap-8 mb-36 md:mb-28 mt-6 md:mt-20 relative bg-neutral-white lg:grid lg:grid-rows-1 col-span-8 lg:col-span-12 lg:grid-cols-12 px-8 md:px-12 lg:px-16 xl:px-20 max-w-screen-2xl m-auto z-0 ">
 
-      <article role="banner" className=" z-20 relative flex-col align-middle col-span-8 lg:col-span-7 xl:col-span-6 lg:place-self-center">
-        <div className=" z-20 flex flex-row align-center mb-8 place-content-center lg:place-content-start">
-          <Image src={hashtag} alt="" width={32} height={32}  />
-          <h1 className=" z-20 break-words font-semibold text-3xl xsm:text-4xl lg:text-5xl xl:text-6xl font-montserrat">
+        <article role="banner" className=" z-20 relative flex-col align-middle col-span-8 lg:col-span-7 xl:col-span-6 lg:place-self-center">
+          <div className=" z-20 flex flex-row align-center mb-8 place-content-center lg:place-content-start">
+            <Image src={hashtag} alt="" width={32} height={32} />
+            <h1 className=" z-20 break-words font-semibold text-3xl xsm:text-5xl lg:text-6xl xl:text-6xl font-montserrat">
               MyFavoriteSport<span className="sr-only">.</span>
-          </h1>
-        </div>
-        <div className="font-normal text-base mb-6 md:mb-12 z-20">
-          <p className=" z-20 mb-4 leading-8 lg:mb8">
-          Artists  <b>aged 14 to 20</b>  create and upload your art and get your family and friends to vote for your masterpiece.
-          </p>
-          <p className="z-20 mb-4 leading-8 lg:mb8">
-          Sports fans and art lovers vote for the artwork you like the most for it to win the Gold (US$3,000), Silver ($2,000), or Bronze ($1,000). 
-          </p>
-          <p className="z-20 mb-4 leading-8 lg:mb8 "> {/* Use &nbsp to allow grid layout to work properly */}
-          This history-making global contest starts on&nbsp;<b> {formatDate("competitionBegin", "MMMM do")} </b>
-          — the Games kick off in Paris. Artists must act soon to get more public votes. The contest ends on {formatDate("competitionEnd", "MMMM do")}.             
-          Winners receive the cash prizes on&nbsp;{formatDate("winnerAnnounced", "MMMM d, yyyy")}.
-          </p>
-          <p className="z-20 mb-4 leading-8 lg:mb8">
-          This #MyFavoriteSport campaign is launched by ICAF.org, a Washington DC-based charity. The entry fee for artists is $10 but voters register for free. 
-          </p>
-          <p className="z-20 mb-4 leading-8 lg:mb8">
-          Your donation helps unite artists and athletes by reassuring them that their chaotic and divided world has hope.
-          </p>
-
-        </div>
-      </article>
-
-      <figure
-        role="region"
-        aria-label="Bicycle art"
-        className="relative z-10 md:grid col-span-8 md:col-start-2 md:col-span-6 lg:col-span-5 xl:col-span-6 "
-      >
-        <div className="z-10 image-container">
-          <Image className="m-auto" src={HeroHeader} alt=""/>
-          <div className="text-right">
-            Eric Liang, age 11, Illinois
+            </h1>
           </div>
-        </div>
-      </figure>
-    </section>
+          <div className="font-normal text-base mb-6 md:mb-12 z-20">
+            <p className=" z-20 mb-4 leading-8 lg:mb8">
+              Artists aged 14 to 20 create and upload your art and get your family and friends to vote for your masterpiece.
+            </p>
+            <p className="z-20 mb-4 leading-8 lg:mb8">
+              Sports fans and art lovers vote for the artwork you like the most for it to win the Gold (US$3,000), Silver ($2,000), or Bronze ($1,000).
+            </p>
+            <p className="z-20 mb-4 leading-8 lg:mb8 "> {/* Use &nbsp to allow grid layout to work properly */}
+              This historic global art contest begins on&nbsp;<b>July 26<sup>th</sup></b>
+              —when the Games kick off in Paris. Artists must act right away to get more public votes. Judges must vote before {formatDate("competitionEnd", "MMMM do")}&nbsp;
+              when the contest ends. Winners receive the cash prizes on &nbsp;{formatDate("winnerAnnounced", "MMMM d, yyyy")}.
+            </p>
+            <p className="z-20 mb-4 leading-8 lg:mb8">
+              This #MyFavoriteSport campaign is launched by ICAF.org, a Washington DC-based charity. The entry fee for artists is $10 but voters register for free.
+            </p>
+            {/* <p className="z-20 mb-4 leading-8 lg:mb8">
+              Your donation helps unite artists and athletes by reassuring them that their chaotic and divided world has hope.
+            </p> */}
+
+          </div>
+        </article>
+
+        <figure
+          role="region"
+          aria-label="Bicycle art"
+          className="relative z-10 px-10 md:px-24 lg:px-0 md:grid col-span-8 md:col-start-2 md:col-span-6 lg:col-span-5 xl:col-span-6 "
+        >
+          <div className="z-10 image-container">
+
+            <Image className="m-auto rounded-2xl" src={HeroHeader} alt="" />
+            <div className="text-sm text-black italic font-medium font-montserrat text-right">
+              * Eric Liang, age 11, Illinois
+            </div>
+          </div>
+        </figure>
+        <Image src={ranbowLine} alt="" className="absolute inset-x-0 bottom-6 lg:-bottom-6 z-0 object-cover object-center w-full"></Image>
+      </section>
+    </>
   );
 };
--- components/home/get-involved/FlippingCardsStatic.tsx
+++ components/home/get-involved/FlippingCardsStatic.tsx
@@ -1,13 +1,13 @@
 "use client";
 import React from "react";
-import {FlippingCardStatic} from "./FlippingCardStatic";
+import { FlippingCardStatic } from "./FlippingCardStatic";
 import monitorUp from "../../../public/svgs/monitor-up.svg";
 import vote from "../../../public/svgs/vote.svg";
 import heart from "../../../public/svgs/heart-handshake.svg";
 import clipboard from "../../../public/svgs/clipboard-edit.svg";
 // import littleHeart from "../../../public/home/get-involved/heart.svg";
 import Image from "next/image";
-import {ButtonStyledLink} from "../../common/ui/ButtonStyledLink";
+import { ButtonStyledLink } from "../../common/ui/ButtonStyledLink";
 import { Pm } from "../../common/texts/Pm";
 import highlightSwimmer from "../../../public/home/Highlight_Swimmer.svg";
 
@@ -21,16 +21,17 @@ export const FlippingCardsStatic = () => {
       </div>
 
       <FlippingCardStatic
-        heading1="View & Vote"
+        heading1="Register"
         heading2="All sports fans and art lovers"
         description={
           <div className="w-full">
             <Pm className=" mt-4 md:mt-12 mb-4 font-light text-base">
-              Search your favorite artwork by sports category or country.
+
+            It's easy to register and completely free!
             </Pm>
-            {/* <Pm className="font-light text-base mb-4 md:mb-0">
+            <Pm className="font-light text-base mb-4 md:mb-0">
               You must register to upload your artwork or to vote for your favorite artist or artwork.
-            </Pm> */}
+            </Pm>
           </div>
         }
         icon={clipboard}
@@ -48,11 +49,11 @@ export const FlippingCardsStatic = () => {
       </FlippingCardStatic>
       <FlippingCardStatic
         heading1="Create & Share"
-        heading2="Artists, aged 14 to 20"
+        heading2="Artists, ages 14-20: Share your creations!"
         description={
           <div>
             <Pm className="mt-4 md:mt-12 mb-4 font-light text-sm md:text-base">
-              Unleash your creativity by producing art on your favorite sport in any medium for upload.
+              Unleash your creativity by painting or drawing your favorite Olympic Sportss. We accept digital or AI art.
             </Pm>
             {/* <Pm className="font-light text-sm md:text-base mb-2 md:mb-4">
               Register, Upload, and Share to get votes.
@@ -63,21 +64,16 @@ export const FlippingCardsStatic = () => {
         color="#EE2F4D"
       >
         <div className="flex-grow"></div> {/* Spacer element */}
-        <ButtonStyledLink
-          href={"/auth/register"}
-          className="my-1 w-full"
-          onTouchStart={(e: React.TouchEvent<HTMLAnchorElement>) => { e.stopPropagation(); }}
-          onClick={(e: React.MouseEvent<HTMLAnchorElement>) => { e.stopPropagation(); }}
-        >
-          Register here
-        </ButtonStyledLink>
       </FlippingCardStatic>
       <FlippingCardStatic
         heading1="Partner"
         description={
-          <div className="w-full">
-            <Pm className=" mt-4 md:mt-12 mb-4 font-light text-base">
-              Join us to reassure young people that their chaotic and divided world has hope.
+          <div>
+            <Pm className="mt-4 md:mt-12 mb-4 font-light text-sm md:text-base">
+              Any 10 to 100-year-old can be a judge.
+            </Pm>
+            <Pm className="font-light text-sm md:text-base">
+              Register first, then search for the artists by name or country, and vote. You can vote only once.
             </Pm>
             {/* <Pm className="font-light text-base mb-4 md:mb-0">
               You must register to upload your artwork or to vote for your favorite artist or artwork.
@@ -98,11 +94,27 @@ export const FlippingCardsStatic = () => {
         </ButtonStyledLink>
       </FlippingCardStatic>
       <FlippingCardStatic
-        heading1="Sponsor"
+        heading1="Sponsor or Donate"
+        heading2="Your support changes lives"
         description={
-          <div className="w-full">
-            <Pm className=" mt-4 md:mt-12 mb-4 font-light text-base">
-              Boost your global emotional branding by winning over young hearts and minds.
+          <div className="flex flex-col justify-between">
+            <Pm className="mt-6 font-light text-sm md:text-base">
+              Help democratize creativity and optimize children's creative potential with your donation today.
+            </Pm>
+
+            <Pm className=" font-light text-sm md:text-base">
+              {"Please "}
+              {/* <a href="https://myfavoritesport.org/sponsor/" target="_blank" rel="noopener noreferrer" className="underline ">
+                here
+              </a>
+              {" for sponsorship."}
+            </Pm>
+            <Pm className="mb-6 font-light text-sm md:text-base">
+              {" "}  */}
+              <a href="https://icaf.org/about/contact-us" target="_blank" rel="noopener noreferrer" className="underline ">
+                contact us
+              </a>
+              {" about partnership prospects."}
             </Pm>
             {/* <Pm className="font-light text-base mb-4 md:mb-0">
               You must register to upload your artwork or to vote for your favorite artist or artwork.
