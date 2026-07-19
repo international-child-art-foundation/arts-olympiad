33b39f3f91dd969984559392c50ed06350ccca6e
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2026-07-19 16:10:09 -0400

    fix: Update end times

--- amplify/backend/function/usersLambda/src/contest.js
+++ amplify/backend/function/usersLambda/src/contest.js
@@ -1,4 +1,4 @@
-const CONTEST_END_TIME = new Date(2026, 6, 18, 24);
+const CONTEST_END_TIME = new Date(2026, 6, 19, 23, 59, 59);
 
 function isVotingClosed(now = new Date()) {
   return now >= CONTEST_END_TIME;
--- mock/dates.ts
+++ mock/dates.ts
@@ -3,7 +3,7 @@ import { format } from "date-fns";
 const dates: { [key: string]: Date } = {
   // Months are 0-indexed
   competitionBegin: new Date(2026, 5, 11, 9),
-  competitionEnd: new Date(2026, 6, 18, 24),
+  competitionEnd: new Date(2026, 6, 19, 23, 59, 59),
   winnerAnnounced: new Date(2026, 6, 27, 11),
 };
 
